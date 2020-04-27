314de5438749304350a6f4fa13b24fd12263ab52
Felipe Carvalho <feliped3carvalho@gmail.com>
2020-04-27 19:39:11 -0300

    Incluido Icons

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Instagram,
+  Twitter,
+}
+
+export default Icons
